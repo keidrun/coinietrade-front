997c1f6d968c8c350c1f44d4c5af7b50c5ca3ddf
keidrun <kssk96@gmail.com>
2018-03-06 21:35:52 -0800

    Add production settings with Docker on Heroku

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /app
 RUN npm install -g yarn nodemon
 
 COPY package.json package.json
+COPY yarn.lock yarn.lock
 RUN yarn install && mv node_modules /node_modules
 
 COPY . .
--- server/src/config/keys.js
+++ server/src/config/keys.js
@@ -2,16 +2,16 @@ require('dotenv').config();
 
 const config = {
   production: {
-    mongoURL: process.env.MONGO_URL,
-    tokenSecret: process.env.TOKEN_SECRET,
-    cookieKey: process.env.COOKIE_KEY,
-    facebookClientId: process.env.FACEBOOK_CLIENT_ID,
-    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
-    googleClientId: process.env.GOOGLE_CLIENT_ID,
-    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
+    mongoURL: process.env.PROD_MONGO_URI,
+    tokenSecret: process.env.PROD_TOKEN_SECRET,
+    cookieKey: process.env.PROD_COOKIE_KEY,
+    facebookClientId: process.env.PROD_FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.PROD_FACEBOOK_CLIENT_SECRET,
+    googleClientId: process.env.PROD_GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.PROD_GOOGLE_CLIENT_SECRET
   },
   integration: {
-    mongoURL: process.env.PRE_MONGO_URL,
+    mongoURL: process.env.PRE_MONGO_URI,
     tokenSecret: process.env.PRE_TOKEN_SECRET,
     cookieKey: process.env.PRE_COOKIE_KEY,
     facebookClientId: process.env.PRE_FACEBOOK_CLIENT_ID,
@@ -20,7 +20,7 @@ const config = {
     googleClientSecret: process.env.PRE_GOOGLE_CLIENT_SECRET
   },
   default: {
-    mongoURL: process.env.DEV_MONGO_URL,
+    mongoURL: process.env.DEV_MONGO_URI,
     tokenSecret: process.env.DEV_TOKEN_SECRET,
     cookieKey: process.env.DEV_COOKIE_KEY,
     facebookClientId: process.env.DEV_FACEBOOK_CLIENT_ID,
