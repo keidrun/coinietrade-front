e50af88ec903e39f7a11cdc93810ce15f4d839b0
keidrun <kssk96@gmail.com>
2018-03-20 21:59:33 -0700

    Add GET ,PUT and DELETE /api/user

--- server/src/routes/userRoutes.js
+++ server/src/routes/userRoutes.js
@@ -2,7 +2,46 @@ const User = require('../models/User');
 const auth = require('../middleware/auth');
 
 module.exports = app => {
-  app.post('/api/user', auth, (req, res) => {});
+  app.get('/api/user', auth, async (req, res) => {
+    const userId = req.user._id;
+    try {
+      const user = await User.findById(userId);
+      res.send(user);
+    } catch (err) {
+      res.status(400).send({
+        error: {
+          message: err.message
+        }
+      });
+    }
+  });
 
-  app.delete('/api/user', auth, (req, res) => {});
+  app.put('/api/user', auth, async (req, res) => {
+    const userId = req.user._id;
+    const newData = req.body;
+
+    try {
+      const updatedUser = await User.findByIdAndUpdate(userId, newData, {
+        new: true
+      });
+      res.send(updatedUser);
+    } catch (err) {
+      res.status(400).send({
+        error: err.message
+      });
+    }
+  });
+
+  app.delete('/api/user', auth, async (req, res) => {
+    const userId = req.user._id;
+
+    try {
+      const deletedUser = await User.findByIdAndRemove(userId);
+      res.send(deletedUser);
+    } catch (err) {
+      res.status(400).send({
+        error: err.message
+      });
+    }
+  });
 };
