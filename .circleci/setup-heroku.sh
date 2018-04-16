4b39398fe883bde9e6dfbc9a36ecf2f95f2a8716
keidrun <kssk96@gmail.com>
2018-04-16 02:07:49 -0700

    Add circleci tasks for dockerize

--- .circleci/setup-heroku.sh
+++ .circleci/setup-heroku.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+readonly HEROKU_CLI_VERSION=v6.16.12-04b3626
+
 wget https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-linux-x64.tar.gz -O heroku.tar.gz
 sudo tar -xvzf heroku.tar.gz
 sudo mkdir -p /usr/local/lib /usr/local/bin
-sudo mv heroku-cli-v6.16.12-04b3626-linux-x64 /usr/local/lib/heroku
+sudo mv heroku-cli-$HEROKU_CLI_VERSION-linux-x64 /usr/local/lib/heroku
 sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
