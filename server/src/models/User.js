04996c2384d145beac461f2531709c239bf2c3dc
keidrun <kssk96@gmail.com>
2018-08-19 23:48:24 -0700

    Add timestamps and tokenExpiredAt to User model

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -2,6 +2,7 @@ const { promisify } = require('util');
 const mongoose = require('mongoose');
 const uuid = require('uuid');
 const jwt = require('jsonwebtoken');
+const moment = require('moment');
 const keys = require('../../config/keys').get(process.env.NODE_ENV);
 
 const { Schema } = mongoose;
@@ -22,72 +23,84 @@ const EXCHANGE_SITES = {
   ZAIF: 'zaif',
 };
 
-const userSchema = new Schema({
-  _id: {
-    type: String,
-    required: true,
-    default: () => uuid.v4(),
-  },
-  authProvider: {
-    name: String,
-    id: {
+const options = {
+  timestamps: true,
+};
+
+const userSchema = new Schema(
+  {
+    _id: {
       type: String,
-      unique: true,
+      required: true,
+      default: () => uuid.v4(),
     },
-    accessToken: String,
-    refreshToken: String,
+    authProvider: {
+      name: String,
+      id: {
+        type: String,
+        unique: true,
+      },
+      accessToken: String,
+      refreshToken: String,
+    },
+    token: String,
+    tokenExpiredAt: Date,
+    displayName: {
+      type: String,
+      minlength: 1,
+      maxlength: 50,
+      required: true,
+      trim: true,
+    },
+    familyName: {
+      type: String,
+      maxlength: 50,
+      trim: true,
+    },
+    givenName: {
+      type: String,
+      maxlength: 50,
+      trim: true,
+    },
+    middleName: {
+      type: String,
+      maxlength: 50,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      unique: true,
+      sparse: true,
+    },
+    avatarUrl: String,
+    gender: {
+      type: String,
+      enum: Object.values(GENDER),
+    },
+    language: {
+      type: String,
+      enum: Object.values(LANGUAGE),
+      default: LANGUAGE.ENGLISH,
+    },
+    secrets: {
+      type: Array,
+      default: [],
+    },
   },
-  token: String,
-  displayName: {
-    type: String,
-    minlength: 1,
-    maxlength: 50,
-    required: true,
-    trim: true,
-  },
-  familyName: {
-    type: String,
-    maxlength: 50,
-    trim: true,
-  },
-  givenName: {
-    type: String,
-    maxlength: 50,
-    trim: true,
-  },
-  middleName: {
-    type: String,
-    maxlength: 50,
-    trim: true,
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: true,
-    sparse: true,
-  },
-  avatarUrl: String,
-  gender: {
-    type: String,
-    enum: Object.values(GENDER),
-  },
-  language: {
-    type: String,
-    enum: Object.values(LANGUAGE),
-    default: LANGUAGE.ENGLISH,
-  },
-  secrets: {
-    type: Array,
-    default: [],
-  },
-});
+  options,
+);
 
 userSchema.methods.generateToken = function generateToken() {
   const user = this;
 
   const token = jwt.sign(user._id, keys.tokenSecret);
+  const tokenExpiredAt = moment()
+    .add(1, 'day')
+    .toISOString();
 
   user.token = token;
+  user.tokenExpiredAt = tokenExpiredAt;
 
   return new Promise(async (resolve, reject) => {
     try {
@@ -102,10 +115,16 @@ userSchema.methods.generateToken = function generateToken() {
 userSchema.statics.findByToken = function findByToken(token) {
   const user = this;
 
+  const now = moment().toISOString();
+
   return new Promise(async (resolve, reject) => {
     try {
       const decodedId = await verifyPromise(token, keys.tokenSecret);
-      const foundUser = await user.findOne({ _id: decodedId, token });
+      const foundUser = await user.findOne({
+        _id: decodedId,
+        token,
+        tokenExpiredAt: { $gte: now },
+      });
       resolve(foundUser);
     } catch (error) {
       reject(error);
@@ -118,7 +137,12 @@ userSchema.methods.deleteToken = function deleteToken() {
 
   return new Promise(async (resolve, reject) => {
     try {
-      const updatedUser = await user.update({ $unset: { token: 1 } });
+      const updatedUser = await user.update({
+        $unset: {
+          token: 1,
+          tokenExpiredAt: 1,
+        },
+      });
       resolve(updatedUser);
     } catch (error) {
       reject(error);
