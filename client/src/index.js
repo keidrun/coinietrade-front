24c2ed9dbb502e155375c0a802a8c6e8e1579526
keidrun <kssk96@gmail.com>
2018-09-01 06:38:29 -0700

    Unreguster serviceWorker

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { unregister } from './registerServiceWorker';
 
 import Routes from './components/Routes';
 
@@ -15,3 +16,5 @@ ReactDOM.render(
   </Provider>,
   document.querySelector('#root'),
 );
+
+unregister();
